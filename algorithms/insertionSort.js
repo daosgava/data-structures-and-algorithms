a42d299ec9845b2c2d6cc1b4c7738558f435077e
daosgava <daosgava.garcia@gmail.com>
2021-11-10 18:47:04 -0500

    improving doc

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -1,94 +1,100 @@
 /*
     Insertion Sort
 
-    1.  Iterate over the input array starting from position 1
+    The idea is to iterate over the input array starting from index 1.
+    In each iteration, we need to compare and sort all the numbers to
+    left of our iteration number(arr[i]).
+
+    1.  Iterate over the input array starting from index 1
         arr = [1,2,1]
-        index = 1
+        arr[index] = 2
     2.  Set 2 variables in each iteration. 
-        2.1 One variable holds the iteration number or arr[i]. Let's call it tmpHolder.
-            tmpHolder = arr[index] = 2
+        2.1 One variable holds the iteration number / arr[i]. Let's call it tmpHolder.
+            tmpHolder = arr[index]
 
-        2.2 The other variable tracks the index of the number inmidiately to the left of the tmpHolder.
-            Let's call it trackedPosition.
-            trackedPosition = i - 1 = 0        
+        2.2 And another variable that tracks the index of the number inmidiately to the left of
+            our current index. Let's call it leftPos.
+            leftPos = i - 1 = 0        
 
-    3.  With an indented loop, compare the tmpHolder to the number in the trackedPosition.
+    3.  With an inner loop, compare the tmpHolder to the number in the leftPos.
         [1,2,1]
          ^ ^
         Comparison 1 > 2? NOP!
 
-        3.1 If the number in the trackedPosition is greater than the tmpHolder
-            3.1.1 Move that arr[trackedPosition] to the right. arr[trackedPosition + 1] = arr[trackedPosition].
-            3.1.2 Move trackedPosition to the left. trackedPosition - 1.
+        3.1 If the number in the leftPos is greater than the tmpHolder
+            3.1.1 Move it to the tmpHolder position, meaning one to the right. arr[leftPos + 1] = arr[leftPos].
+            3.1.2 Move leftPos to the left -> leftPos - 1. Next number to the left.
         
-        3.2 If conditional is false, break the indented loop
-        NOTE: The indented loop will sort all the number to the left of the current iteration number.
+        3.2 If conditional is false, break the inner loop
+        NOTE: The inner loop will sort all the numbers to the left of the current iteration number.
 
-    4.  At the end of each iteration, assign tmpHolder to trackedPosition + 1.
+
+    4.  Before the outer loop finishes that run, assign tmpHolder to leftPos + 1.
     5.  Repeat until reaching the last number in the array.
-    6.  Returns input array.
-    
-
+    6.  Finally, Returns input array.
 
     e.g.
     inputArray = [1,2,1,5]
 
-    Iteration 1
+    Outer loop
+    - Iteration 1
        2
     [1, ,1,5]
-    index = 1, tmpHolder = 2, trackedPosition = 0
-        Indented loop
+    index = 1, tmpHolder = 2, leftPos = 0
+        Inner loop
         Iteration 1
-        1 > 2 NOP! Then break loop and add tempHolder number to trackedPosition + 1
+        1 > 2 NOP! Then break loop, add tempHolder number to leftPos + 1, continue outer loop
 
-    Iteration 2
+    - Iteration 2
          1	
     [1,2, ,5]
-    index = 2, tmpHolder = 1, trackedPosition = 1
-        Indented loop
+    index = 2, tmpHolder = 1, leftPos = 1
+        Inner loop
         Iteration 1
-        2 > 1 YES! Then move 2 to the right and move trackedPosition to the left.
+        2 > 1 YES! Then move 2 to the right and move leftPos to the left.
         [1, ,2,5]
-        trackedPosition = 0
+        leftPos = 0
 
         Iteration 2
-        1 > 1 Nop!
-    Then add tmpHolder value to trackedPosition + 1
+        1 > 1 Nop! -> Then break the inner loop.
+    Place tmpHolder in leftPos + 1
     [1,1,2,5]
     
-    Iteration 3
+    - Iteration 3
            5      
     [1,1,2, ]
-    index = 3, tmpHolder = 5, trackedPosition = 2
-        Indented loop
+    index = 3, tmpHolder = 5, leftPos = 2
+        Inner loop
         Iteration 1
         2 > 5! Nop! Then break. 
-    Add tmpHolder number to trackedPosition + 1
+    Add tmpHolder number to leftPos + 1
 
     Return inputArray
 */
 
 const insertionSort = (arr) => {
     let tempHolder = null;
-    let trackedPosition = null;
+    let leftPos = null;
     for(let i = 1; i < arr.length; i++){
         tempHolder = arr[i];
-        trackedPosition = i - 1;
+        leftPos = i - 1;
 
-        while(trackedPosition >= 0) {
-            if(arr[trackedPosition] > tempHolder){
-                arr[trackedPosition + 1] = arr[trackedPosition];
-                --trackedPosition;
+        while(leftPos >= 0) {
+            if(arr[leftPos] > tempHolder){
+                arr[leftPos + 1] = arr[leftPos];
+                --leftPos;
             } else {
                 break;
             }
         }
         
-        arr[trackedPosition + 1] = tempHolder;
+        arr[leftPos + 1] = tempHolder;
     }
 
     return arr;
 
 };
+
+// Time complexity O(n^2)
 
 module.exports = insertionSort;
--- algorithms/selectionSort.js
+++ algorithms/selectionSort.js
@@ -1,7 +1,9 @@
 /*
     Selection Sort
 
-    This algorithm sorts an array in ascending order.
+    This algorithm sorts an array in ascending order. The idea is to place the smallest number
+    at the beginning of the array, yet we'll be moving the lower bound to the right after each iteration.
+
     Input array [2,4,2,7,1]
 
     1.  The idea is to pass-through the input array.
@@ -9,20 +11,19 @@
         [2,4,2,7,1]
         indexOfLowestNum = 0
         array[0] = 2
-    3.  Look for the lowest number and track its position(passing-through the array again).
+    3.  With an inner loop, look for the lowest number and track its position.
         2 > 4 NO
         2 > 2 NO
         2 > 7 NO
         2 > 1 YES
-        The index of the number 1 is 4
-        indexOfLowestNum = 4
-    4.  Once we find lowest number, it should change positions with the initially set up index of lowest number
-        Initial indexOfLowestNum = 0
-        The actual indexOfLowestNum = 4
-        Let's change numbers
+        The index of number 1 is 4 -> arr[4]
+    4.  Right after the inner loop's run, we need to move the actual lowest number to indexOfLowestNum.
+        indexOfLowestNum = 0
+        Actual lowest number arr[4] -> 1
+        Now, let's change numbers
         [1,4,2,7,2]
     5.  The indexOfLowestNum will change on each iteration, starting with 0, then 1, then 2, and so on.
-    6.  loop should go until the second last position.
+    6.  Loop will iterate until the second last position.
 
     Initial indexOfLowestNum = I
     The actual indexOfLowestNum = T
