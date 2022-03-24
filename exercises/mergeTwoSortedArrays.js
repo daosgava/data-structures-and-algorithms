24e8468c62488e05d8a1a1b2621fed1f82566110
Daniel García Vargas <daosgava.garcia@gmail.com>
2022-03-23 21:31:16 -0500

    Update mergeTwoSortedArrays.js

--- exercises/mergeTwoSortedArrays.js
+++ exercises/mergeTwoSortedArrays.js
@@ -2,7 +2,7 @@
 	m: represents the num1s length
 	n: represents the num2s length 
 
-    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    	Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 	Output: [1,2,2,3,5,6]
 
 	Note: The idea for this exercise is to overwrite nums1
